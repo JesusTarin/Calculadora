ead08ad42ca6e3322b3188fea3080126246653d5
jtarinia <jestarale@alu.edu.gva.es>
2024-04-10 20:59:28 +0200

    0.4

--- app/src/main/java/es/ieslavereda/calculadora/MainActivity.java
+++ app/src/main/java/es/ieslavereda/calculadora/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView display;
     private Double operando;
     private Operacion operacion;
+    private CheckBox opciones;
 
 
     @Override
